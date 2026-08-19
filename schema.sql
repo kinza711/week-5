f5f22a33931dae99a4d697010a139e8f88841b52
kinza <kinzashahzadi7181@gmail.com>
2026-08-19 21:54:14 +0500

    feat: add schema value constraints (w-2)

--- schema.sql
+++ schema.sql
@@ -1,5 +1,5 @@
--- create user table 
 
+-- craete user tabel
 create table users (
 id INTEGER generated always as identity primary KEY,
 name text not NULL,
@@ -7,48 +7,47 @@ email text not null unique,
 created_at timestamptz default now()
 );
 
--- create project table 
+-- create projects table
+
 CREATE TABLE projects (
     id INTEGER GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
     name TEXT NOT NULL,
-    owner_id INTEGER,
+    owner_id INTEGER not Null,
     created_at TIMESTAMPTZ DEFAULT NOW()
 );
 
--- create tags table 
+-- -- craete tags table
 create table tags(
     id INTEGER generated always as identity primary key,
      name text UNIQUE NOT NULL
 );
 
--- create project-members table 
+-- -- craete project_members tabel 
 create table project_members(
-    user_id INTEGER,
-    project_id INTEGER,
-    role TEXT NOT NULL 
+    user_id INTEGER not Null,
+    project_id INTEGER not Null,
+    role TEXT NOT NULL check (role in ('owner' , 'admin' , 'member' , 'viewer'))
 );
- 
- -- create tasks table 
+-- --carete task table 
 create table tasks(
     id INTEGER generated always as identity primary key,
     title text NOT NULL,
     description text ,
-    status text ,
-    priority integer,
+    status text NOT NULL check (status in ('todo' , 'in_progress' , 'done')),
+    priority integer NOT NULL check (priority between 1 and 5) ,
     project_id INTEGER  NOT NULL,
     assignee_id integer NULL,
     due_date timestamptz,
     created_at  timestamptz default now()
 );
 
--- create task-tags table 
+-- --craete task-tag table 
 create table task_tags(
     task_id integer ,
-    tag_id integer 
-     
+    tag_id integer   
 );
 
--- create comments table 
+-- carete comments table
 create table comments(
     id INTEGER generated always as identity primary key,
     task_id integer NOT NULL,
