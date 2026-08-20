4a518d19b213ddf3c5dbb5286f651ac68c5b4038
kinza <kinzashahzadi7181@gmail.com>
2026-08-20 11:50:18 +0500

    feat: user seeds added to users table (c-3)

---
+++ seed.sql
@@ -0,0 +1,13 @@
+-- ============ C 3  Seed Data =============
+
+-- user data seeds
+
+INSERT INTO users (name, email)
+VALUES
+    ('Asad', 'asad@gmail.com'),
+    ('faiza', 'faiza@gmail.com'),
+    ('bilal', 'bilal@gmail.com'),
+    ('Ansa', 'ansa@gmail.com'),
+    ('Muneeba Tariq', 'muneeba.tariq@gmail.com'),
+    ('Kinza', 'kinza@.gmailcom');
+
