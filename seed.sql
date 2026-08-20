bbf7415273b55e4846ee29bdf9ccd03a35172b7f
kinza <kinzashahzadi7181@gmail.com>
2026-08-20 13:02:29 +0500

    feat: projects_member seeds added to users table (c-3)

--- seed.sql
+++ seed.sql
@@ -17,3 +17,17 @@ VALUES
     ('Website Redesign', 1),
     ('Mobile Application', 2),
     ('Internal Dashboard', 3);
+
+-- projects members seeds 
+INSERT INTO project_members (user_id, project_id, role)
+VALUES
+    (1, 1, 'owner'),
+    (2, 1, 'admin'),
+    (3, 1, 'member'),
+
+    (2, 2, 'owner'),
+    (4, 2, 'admin'),
+    (5, 2, 'member'),
+
+    (3, 3, 'owner'),
+    (6, 3, 'viewer');
