bf7d075748cb6d679e5c85bf9947f1dda70ab2b7
kinza <kinzashahzadi7181@gmail.com>
2026-08-20 13:00:16 +0500

    feat: projects seeds added to users table (c-3)

--- seed.sql
+++ seed.sql
@@ -11,3 +11,9 @@ VALUES
     ('Muneeba Tariq', 'muneeba.tariq@gmail.com'),
     ('Kinza', 'kinza@.gmailcom');
 
+-- projects seeds data
+INSERT INTO projects (name, owner_id)
+VALUES
+    ('Website Redesign', 1),
+    ('Mobile Application', 2),
+    ('Internal Dashboard', 3);
