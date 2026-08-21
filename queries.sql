a83b71aa8b9359d022674a2b4dde36f68a39fc36
kinza <kinzashahzadi7181@gmail.com>
2026-08-21 12:02:49 +0500

    feat: add task status count query (w-2/Q2)

--- queries.sql
+++ queries.sql
@@ -1,5 +1,12 @@
 -- Q1: All tasks for one project, ordered by due date ascending with NULL due dates last.
 
-SELECT * FROM tasks
-WHERE project_id = 1
-ORDER BY due_date ASC NULLS LAST;
+-- SELECT * FROM tasks
+-- WHERE project_id = 1
+-- ORDER BY due_date ASC NULLS LAST;
+
+-- Q2: Count the number of tasks in each status.
+
+SELECT status, COUNT(*)
+FROM tasks
+GROUP BY status;
+
