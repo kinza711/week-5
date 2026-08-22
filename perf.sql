64a6b68313f8334613cfa83cdd43424c91d1d7ee
kinza <kinzashahzadi7181@gmail.com>
2026-08-22 16:36:41 +0500

    perf: w-2 additional indexes added

--- perf.sql
+++ perf.sql
@@ -2,5 +2,14 @@
 -- queries that filter or join tasks by project_id.
 
 create index idx_tasks_project_id
-on tasks(project_id)
+on tasks(project_id);
 
+
+-- W2: Create 2 more index on tasks.status , tasks.assignee_id , or on task_tags 
+-- queries that filter or join tasks.
+
+CREATE INDEX IF not EXISTS idx_tasks_status
+ON tasks(status);
+
+create index if not exists idx_tasks_assignee_id
+on tasks(assignee_id);
